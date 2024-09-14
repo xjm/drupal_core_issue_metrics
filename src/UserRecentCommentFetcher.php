652c8542c878d535c68f59d78c442360658bfe4e
xjm <xjm@65776.no-reply.drupal.org>
2024-09-14 14:10:15 -0500

    Inject recent comment timeframe rather than hardcoding it.

--- src/UserRecentCommentFetcher.php
+++ src/UserRecentCommentFetcher.php
@@ -10,6 +10,15 @@ use GuzzleHttp\Exception\BadResponseException;
  */
 class UserRecentCommentFetcher extends FetcherBase {
 
+  public function __construct(protected RequestInterface $requestGroup, protected Client $client, protected int $oldestCommentData = 0) {
+
+    if (empty($this->oldestCommentData)) {
+      // Fetch at least three months of data by default so we don't miss items
+      // if we run reports for specific timeframes.
+      $this->oldestCommentData = strtotime('3 months ago');
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -18,13 +27,9 @@ class UserRecentCommentFetcher extends FetcherBase {
     // Get the last comment on the page.
     $lastComment = $page->list[sizeof($page->list) - 1];
 
-    // Fetch at least three months of data so we don't miss items if we run
-    // reports for specific timeframes.
-    $threeMonthsAgo = strtotime('3 months ago');
-
     // The fetch is complete if the last comment is older than 3 months, or
     // incomplete otherwise.
-    return $threeMonthsAgo >= $lastComment->created;
+    return $this->oldestCommentData >= $lastComment->created;
   }
 
 }
